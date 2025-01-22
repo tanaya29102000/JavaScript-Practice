4932e0edf3f3cfe6311739cb038cb6ffef83ee99
Tanaya <tanayakanerkar@gmail.com>
2025-01-22 13:59:28 +0530

    first js commit

---
+++ Datatypes.js
@@ -0,0 +1,6 @@
+"user strict"; //treat all js code as newer version 
+
+//alert(3+3) we are using nodejs,not browser 
+
+console.log(3+3); console.log("Httesh")
+
---
+++ Variable.js
@@ -0,0 +1,24 @@
+const accountId = 12345
+
+let accountEmail = "tanay@gmail.com"
+
+var accountPassword = "1236"
+
+accountCity = "Jaipur"
+
+//accountId = 2
+//Assignment to constatnt variable.
+
+accountEmail ="dbfj@fg.com"
+accountPassword ="546575"
+accountCity = "delhi";
+//Semicolon is not manadatory in javascript
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+//two ways to output console.log and console.table.
+console.log(accountId);
+
+// {} is called scope variable 
+
+//always use let and const do not prefer var,bcoz of issue in blcok scope and functinal scope. 
+
