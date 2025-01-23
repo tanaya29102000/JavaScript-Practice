fba56ccd5f278b05466145d02f1af23b63ce0693
Tanaya <tanayakanerkar@gmail.com>
2025-01-23 22:46:12 +0530

    hggu

--- DataTypeSummery.js
+++ DataTypeSummery.js
@@ -3,7 +3,33 @@
 
 
 const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id=== anotherId);
+
+const bigNumber = 345465755868n
+
 //Referenced Type (Non primitive) DataType
 //Array, Objects, Functions,
   
+const heros = ["shaktiman", "bahubali"]
+let myObj =
+{
+    name:"tanaya",
+    age: 24,
 
+}
+
+const myFunction = function(){
+console.log("hi tanay");
+}
+
+console.log(typeof scoreValue);
+
+//*****************************************************//
+//  */
