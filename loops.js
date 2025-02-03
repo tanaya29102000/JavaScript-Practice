30a624dabbdc21983e353f1da91dc5898743c812
Tanaya <tanayakanerkar@gmail.com>
2025-02-03 15:40:40 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,39 @@
+//LOOPS
+
+//FOR LOOP ->If you know how many times
+for(let i= 1; i<= 10; i = i+1){
+    console.log('tanaya'+ i)
+}
+
+
+//WHILE LOOP -> In while loop first check condition then execute code .
+let ip = 50
+let house = 50
+
+//While(ip != house){
+//ip =ip+1;
+//console.log('step Taken' + ip}
+//}
+
+//DO WHILE LOOP -> In Do While loop first run the code and then check condition
+//do{
+//ip = ip + 1;
+//console.log('Step Taken' + ip)
+//}while(ip<= house)
+
+//Guesss the input 
+
+let number = 40;
+
+let guess = 0;
+
+do{
+    guess = parseInt("guess a number")
+    if (guess == number){
+
+    alert('winner')
+    break;
+}
+}while(guess!=0){
+    
+}
