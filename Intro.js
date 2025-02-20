efeacc49f6f41ab831267774971e1a18e6cbaafc
Tanaya <tanayakanerkar@gmail.com>
2025-02-20 14:25:50 +0530

    intro

---
+++ Intro.js
@@ -0,0 +1,9 @@
+//JavaScript is just a programming language for web applications.
+//NOTE: Javascript has nothing to do with Java
+//EXAMPLE : Car and Carpet 
+//JAVA and Javascipt
+
+//Internal Linking and External Linking 
+//Write the javascript code inside the html file.
+//Write the javascript code in seprate.js file 
+
